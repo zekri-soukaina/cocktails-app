24c059284b48b6db06fc5fabaed4e74ed24d304d
zekri-soukaina <zekrisoukaina@gmail.com>
2021-04-07 17:05:09 +0200

    npm run build to check before deploying

--- src/Components/CocktailCard.tsx
+++ src/Components/CocktailCard.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Button, Card, ListGroup } from "react-bootstrap";
+import { Button, Card } from "react-bootstrap";
 import { Link } from "react-router-dom";
 
 export type Drinks = {
