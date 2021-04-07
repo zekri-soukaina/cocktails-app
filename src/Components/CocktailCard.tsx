fcedb2d2586c6d57a1da64627be6128af81b5135
zekri-soukaina <zekrisoukaina@gmail.com>
2021-04-07 16:45:47 +0200

    compenent cocktails simpifiy and reuse it for other pages

---
+++ src/Components/CocktailCard.tsx
@@ -0,0 +1,35 @@
+import React from "react";
+import { Button, Card, ListGroup } from "react-bootstrap";
+import { Link } from "react-router-dom";
+
+export type Drinks = {
+  idDrink: string;
+  strDrink: string;
+  strDrinkThumb: string;
+};
+type Props = {
+  drink: Drinks;
+};
+export default function CocktailCard({ drink }: Props) {
+  return (
+    <div style={{ margin: "20px" }} key={drink.idDrink}>
+      <Card style={{ width: "18rem" }}>
+        <Card.Img
+          variant="top"
+          src={drink.strDrinkThumb}
+          alt={drink.strDrink}
+        />
+        <Card.Body>
+          <Card.Title>{drink.strDrink}</Card.Title>
+          {/* <Card.Text>
+          Some quick example text to build on the card title and make up
+          the bulk of the card's content.
+        </Card.Text> */}
+          <Link to={`/cocktail_info/${drink.idDrink}`}>
+            <Button variant="primary">Let's make it happen</Button>
+          </Link>
+        </Card.Body>
+      </Card>
+    </div>
+  );
+}
