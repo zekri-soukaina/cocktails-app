0250e57b08f1f887bb2b87ff0d2e36afe34c0ce5
zekri-soukaina <zekrisoukaina@gmail.com>
2021-04-08 12:12:58 +0200

    rerdirect file

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { Route, Switch } from "react-router-dom";
 import Alcoholic from "./Pages/Alcoholic";
 import SearchPage from "./Pages/SearchPage";
 import HomePage from "./Pages/HomePage";
@@ -10,18 +10,15 @@ import MenuPage from "./Pages/MenuPage";
 
 function App() {
   return (
-    <Router>
-      <Switch>
-        <Route exact path="/" component={HomePage} />
-        <Route path="/noalcoholic" component={NoAlcoholic} />
-        <Route path="/alcoholic" component={Alcoholic} />
-
-        <Route path="/list/:searchText+" component={ListCategories} />
-        <Route path="/list" component={ListCocktails} />
-        <Route path="/cocktail_info/:idDrink" component={MenuPage} />
-        <Route path="/category/:searchText?" component={SearchPage} />
-      </Switch>
-    </Router>
+    <Switch>
+      <Route exact path="/" component={HomePage} />
+      <Route path="/noalcoholic" component={NoAlcoholic} />
+      <Route path="/alcoholic" component={Alcoholic} />
+      <Route path="/list/:searchText+" component={ListCategories} />
+      <Route path="/list" component={ListCocktails} />
+      <Route path="/cocktail_info/:idDrink" component={MenuPage} />
+      <Route path="/category/:searchText?" component={SearchPage} />
+    </Switch>
   );
 }
 
